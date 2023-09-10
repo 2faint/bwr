0fbc58b088e52d53060de01cc295fa05bd8b023e
2faint <72884881+2faint@users.noreply.github.com>
2023-09-10 14:11:30 +0200

    Create MAINSCRIPT.lua

---
+++ MAINSCRIPT.lua
@@ -0,0 +1,12 @@
+--[[
+	faint.byp
+	Version 2.00 BETA
+	Made by 2faint
+]]
+
+onlycustom=false -- only show custom bypasses
+canvasSize=60 -- change if custom bypasses dont show up
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/2faint/faintbyp/main/faintbypv2%20obf'),true))()
+
+-- add_bypass("Lorem Ipsum")
